b7f299769db4cf11f6ab278f4d563b12c5683d99
grabowski74 <matheus.araujo@ccc.ufcg.edu.br>
2020-01-28 15:45:52 -0300

    add loop

--- Chapter 2 - Linked Lists/loopDetection.py
+++ Chapter 2 - Linked Lists/loopDetection.py
@@ -2,3 +2,25 @@ from linkedListSupport import LinkedList
 from linkedListSupport import Node
 
 # Natural Solution
+
+def findBeggining(head):
+    slow = head
+    fast = head
+
+    while(fast != None and fast.next != None):
+        slow = slow.next
+        fast = fast.next.next
+
+        if(slow == fast):
+            break
+
+    if(fast == None or fast.next == None):
+        return None
+
+    slow = head
+    while(slow != fast):
+        slow = slow.next
+        fast = fast.next
+
+    return fast
+    
